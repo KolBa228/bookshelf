8ce1b69d26d2385cbc021d50467373d8e7ed7c65
maryna-haliieva <marina.galieva7@gmail.com>
2023-07-08 00:04:04 +0300

    update markup

--- src/js/popup/createMarkup.js
+++ src/js/popup/createMarkup.js
@@ -9,11 +9,29 @@ function createMarkup(product) {
         <h2 class="book-modal-title">${product.title}</h2>
         <h3 class="book-modal-author">${product.author}</h3>
         <p class='book-modal-desc'>${product.description}</p>
+        <ul class='icon-book-modal-list'>
+        <li>
+            <a href="" target="_blank">
+            <img src="./img/1-amazon.jpg" alt="amazon">
+            </a>
+        </li>
+        <li>
+            <a href="" target="_blank">
+            <img src="./img/2-ibook.jpg" alt="ibook">
+            </a>
+        </li>
+        <li>
+            <a href="" target="_blank">
+            <img src="./img/3-bookshop.jpg" alt="bookshop">
+            </a>
+        </li>
+        </ul>
     </div>
+
+    <button type="button">Add to shopping list</button>
     </div>`
   );
   instance.show();
 }
 
-console.log(createMarkup);
 export { createMarkup };
