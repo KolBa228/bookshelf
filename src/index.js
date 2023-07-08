91e67a53d6882f66c56e0caeb10c2c7a9852a0ff
MizakaMaks <mizerniy.maksim@ukr.net>
2023-07-08 17:10:40 +0200

    fix: mark-up

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import getCategoryList from './js/service/getCategoryList';
-
 import './js/categories_list';
-import categoriesList from './js/categories_list';
 import { suppMarkup } from './js/create_supp_items';
 import { suppCases } from './js/create_supp_items';
 import './js/supp_swiper';
 import { createMarkup } from './js/popup/createMarkup';
+import fillCategories from './js/categories_list';
 
 const button = document.querySelector('.test');
 
 suppMarkup();
 
-categoriesList()
+fillCategories()
--- src/js/categories_list.js
+++ src/js/categories_list.js
@@ -4,14 +4,14 @@ const fillCategories = async() => {
     const categories = await getCategoryList()
 console.log(categories)
 const tempHTML = categories.map(category =>{
-    `
+    return( `
     <li class="categories_list_item">
       <a class="categories_link" href="/categories?category=${category.list_name}">
       ${category.list_name}
       </a>
-    </li>`;
+    </li>`)
 })
-categoriesList.innerHTML = tempHTML;
+categoriesList.innerHTML = tempHTML.join(' ');
 }
 export default fillCategories
 
