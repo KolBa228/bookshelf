47ff785142f9e5d1de083a2a140eacc75936bba0
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-07-09 22:33:42 +0300

    finish header section

--- src/js/categories_list.js
+++ src/js/categories_list.js
@@ -1,25 +1,24 @@
 import { categoriesList } from "./querrySelectors"
 import getCategoryList from "./service/getCategoryList"
 import SimpleBar from "simplebar"
-const fillCategories = async() => {
-    const categories = await getCategoryList()
-console.log(categories)
-const buttonHTML = [
+const fillCategories = async () => {
+  const categories = await getCategoryList()
+  const buttonHTML = [
     `<li class="categories-list-item">
       <button class="categories-button all-categories-button" type="button">
       All categories
       </button>
     </li>`
-]
-const tempHTML = categories.map(category =>{
-    return( `
+  ]
+  const tempHTML = categories.map(category => {
+    return (`
     <li class="categories-list-item">
       <button class="categories-button" type="button">
       ${category.list_name}
       </button>
     </li>`)
-})
-categoriesList.innerHTML = tempHTML.join(' ');
+  })
+  categoriesList.innerHTML = tempHTML.join(' ');
 }
 export default fillCategories
 
