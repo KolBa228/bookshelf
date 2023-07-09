6fff86a9788f9e357f89780b3b64e3bbc0d3f072
maryna-haliieva <marina.galieva7@gmail.com>
2023-07-09 22:47:09 +0300

    update pop up

--- src/js/popup/popUpMarkup.js
+++ src/js/popup/popUpMarkup.js
@@ -1,16 +1,13 @@
 import bookCard from '../../temlpates/bookCard.hbs';
+import { backDrop, popUp, ulAll } from '../querrySelectors';
 import getBookById from '../service/getBookById';
 import getBooksByCategory from '../service/getBooksByCategory';
 
-const ulAll = document.querySelectorAll('.best-sellers-books-list');
-
 ulAll.forEach(ul => {
-  ul.addEventListener('click', openPopUp);
+  ul.addEventListener('click', onBookCardClick);
 });
 
-const bookCard = bookCard(getBooksByCategory);
-
-export async function openPopUp(ev) {
+export async function onBookCardClick(ev) {
   ev.preventDefault();
 
   if (ev.target.nodeName === 'LI') {
@@ -18,44 +15,55 @@ export async function openPopUp(ev) {
   }
 
   const bookInfo = await getBookById(ev.target.id);
+  //   console.log(ev.target.id);
+
   console.log(bookInfo);
 
-  //   getBookById(ev.target.id).then(res => {
-  //     console.log(res);
-  //   });
+  const popUpItemMarkup = `<div class='book-modal-container'>
+  <img src="${bookInfo.book_image}" alt="${bookInfo.title}" class="book-modal-img"/>
+  
+    <div class='book-modal-details'>
+        <h2 class="book-modal-title">${bookInfo.title}</h2>
+        <h3 class="book-modal-author">${bookInfo.author}</h3>
+        <p class='book-modal-desc'>${bookInfo.description}</p>
+        <ul class='icon-book-modal-list'>
+        <li>
+            <a href="" target="_blank">
+            <img src="https://i.ibb.co/vvPnCJ6/1-amazon.png" alt="amazon">
+            </a>
+        </li>
+        <li>
+            <a href="" target="_blank">
+            <img src="https://i.ibb.co/nj6G7gJ/2-ibook.png" alt="ibook">
+            </a>
+        </li>
+        <li>
+            <a href="" target="_blank">
+            <img src="https://i.ibb.co/fFPnVJN/3-bookshop.png" alt="bookshop">
+            </a>
+        </li>
+        </ul>
+    </div>
+    </div>
+
+    <button type="button" class='book-modal-btn js-add' id='js-book-modal-btn'>Add to shopping list</button>
+
+    <p class='book-modal-buy'>Сongratulations! You have added the book to the shopping list. To delete, press the button “Remove from the shopping list”.</p>
+    <button class='book-modal-close' id='js-book-modal-btn-close'>
+    <svg class='icon-book-modal-close'><use href='./img/symbol-defs.svg#icon-close'></use></svg>
+    </button>`;
+
+  popUp.innerHTML = popUpItemMarkup;
+
+  console.log(popUpItemMarkup);
+
+  const addToShopListButton = document.querySelector('.book-modal-btn');
+  addToShopListButton.addEventListener(
+    'click',
+    localStorage.setItem('bookList', JSON.stringify(bookInfo))
+  );
+
+  backDrop.classList.remove('hidden');
+  popUp.classList.remove('hidden');
+  document.body.style.overflow = 'hidden';
 }
-
-// const popUpMarkup = `<img src="${product.book_image}" alt="${product.title}" class="book-modal-img"/>`;
-
-// const popMarkup = `<div class="book-modal">
-//     <img src="${product.book_image}" alt="${product.title}" class="book-modal-img"/>
-//     <div class='book-modal-details'>
-//         <h2 class="book-modal-title">${product.title}</h2>
-//         <h3 class="book-modal-author">${product.author}</h3>
-//         <p class='book-modal-desc'>${product.description}</p>
-//         <ul class='icon-book-modal-list'>
-//         <li>
-//             <a href="" target="_blank">
-//             <img src="./img/1-amazon.jpg" alt="amazon">
-//             </a>
-//         </li>
-//         <li>
-//             <a href="" target="_blank">
-//             <img src="./img/2-ibook.jpg" alt="ibook">
-//             </a>
-//         </li>
-//         <li>
-//             <a href="" target="_blank">
-//             <img src="./img/3-bookshop.jpg" alt="bookshop">
-//             </a>
-//         </li>
-//         </ul>
-//     </div>
-
-//     <button type="button" class='book-modal-btn js-add' id='js-book-modal-btn'>Add to shopping list</button>
-
-//     <p class='book-modal-buy'>Сongratulations! You have added the book to the shopping list. To delete, press the button “Remove from the shopping list”.</p>
-//     <button class='book-modal-close' id='js-book-modal-btn-close'>
-//     <svg class='icon-book-modal-close'><use href='./img/symbol-defs.svg#icon-close'></use></svg>
-//     </button>
-//     </div>`;
