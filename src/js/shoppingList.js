1b3d3b12678a5c55ae2f02c76ca31392e37b1a83
Anna Samoilova <samoilova0299@gmail.com>
2023-07-10 22:41:08 +0200

    add empty page

--- src/js/shoppingList.js
+++ src/js/shoppingList.js
@@ -3,46 +3,53 @@ import getBookById from './service/getBookById';
 const KEY_LIST = 'bookList';
 
 const listEl = document.querySelector('.books-shoppingList');
+let imgEmptyLarge = new URL('../img/emptyLarge@2x.png', import.meta.url);
+let imgEmpty = new URL('../img/emptySmall.png', import.meta.url);
 
-console.dir(listEl);
+function displayMarkupBasedOnLocalStorage() {
+  const dataEl = localStorage.getItem(KEY_LIST);
+  const parsedDataEl = JSON.parse(dataEl);
 
-const dataEl = localStorage.getItem(KEY_LIST);
-const parsedDataEl = JSON.parse(dataEl);
+  if (parsedDataEl && parsedDataEl.length > 0) {
+    // Display markup when there are items in local storage
+    getBookInfo();
+  } else {
+    // Display alternative markup when local storage is empty
+    const emptyMarkup = markupEmptyPage;
+    listEl.innerHTML = emptyMarkup;
+  }
+}
 
-function markupBooks(parsedDataEl) {
-  listEl.innerHTML = '';
-  const markupItem = parsedDataEl
-    .map(parsedDataEl => {
-      return `
-      <li class="shoppingList-item">
-        <img class="shoppingList-img" src="./img/book-testImg.png" alt="" />
+function markupBooks(data) {
+  const dataArray = Array.isArray(data) ? data : [data];
+  let markupItem = '';
+  for (const book of dataArray) {
+    markupItem += `<li class="shoppingList-item">
+        <img class="shoppingList-img" src=${book.book_image} alt="" />
         <div class="shoppingList-container">
           <div id="content" class="box-shoppingList">
             <div>
-              <h2 class="shoppingList-title">I will find you</h2>
-              <p class="shoppingList-category">Hardcover fiction</p>
+              <h2 class="shoppingList-title">${book.title}</h2>
+              <p class="shoppingList-category">${book.list_name}</p>
             </div>
-            <button class="shoppingList-trash-btn" id="$">
+            <button class="shoppingList-trash-btn" id=${book._id}>
               <svg class="shoppingList-icon-trash">
                 <use href="./img/symbol-defs.svg#icon-trash"></use>
               </svg>
             </button>
+            </button>
           </div>
           <p class="shoppingList-content">
-            David Burroughs was once a devoted father to his three-year-old son
-            Matthew, living a dream life just a short drive away from the
-            working-class suburb where he and his wife, Cheryl, first fell in
-            love--until one fateful night when David woke suddenly to discover
-            Matthew had been murdered while David was asleep just down the hall.
+          ${book.description}
           </p>
           <div class="shoppingList-link-container">
-            <p class="text-shoppingList-author">Harlan Coben</p>
+            <p class="text-shoppingList-author">${book.author}</p>
             <ul class="box-shoppingList-shop">
               <li>
-                <a class="shop-shoppingList-link" href="https://www.amazon.com">
+                <a class="shop-shoppingList-link" href=${book.buy_links[0]}>
                   <img
                     class="shop-shoppingList-img1"
-                    src="./img/1-amazon.jpg"
+                    src="./img/amazon.png"
                     alt=""
                   />
                 </a>
@@ -50,11 +57,11 @@ function markupBooks(parsedDataEl) {
               <li>
                 <a
                   class="shop-shoppingList-link"
-                  href="https://goto.applebooks.apple"
+                  href=${book.buy_links[1]}
                 >
                   <img
                     class="shop-shoppingList-img2"
-                    src="./img/2-ibook.jpg"
+                    src="./img/book.png"
                     alt=""
                   />
                 </a>
@@ -62,26 +69,51 @@ function markupBooks(parsedDataEl) {
               <li>
                 <a
                   class="shop-shoppingList-link"
-                  href="https://du-gae-books-dot-nyt-du-prd.appspot.com"
+                  href=${book.buy_links[4]}
                 >
                   <img
                     class="shop-shoppingList-img2"
-                    src="./img/3-bookshop.jpg"
+                    src="./img/bookshop.png"
                     alt=""
                   />
                 </a>
-              </li>`;
-    })
-    .join('');
-
+              </li> 
+              </ul>
+              </div>
+            </div>
+          </li>`;
+  }
   listEl.innerHTML = markupItem;
 }
 
-const id = '643282b2e85766588626a12a';
+const markupEmptyPage = `<li><p class="shoppingList-text">
+This page is empty, add some books and proceed to order.
+</p>
+<a href="./index.html">
+<picture>
+  <source srcset="${imgEmptyLarge} 2x" type="image/png" />
+  <img
+    class="shoppingList-img-empty"
+    src="${imgEmpty}"
+    alt="Book"
+  />
+</picture>
+</a></li>`;
+
+const dataEl = localStorage.getItem(KEY_LIST);
+
+const parsedDataEl = JSON.parse(dataEl);
 
 const getBookInfo = async () => {
-  const data = await getBookById(id);
-  console.log(data);
+  const bookDataArray = [];
+  for (let i = 0; i < parsedDataEl.length; i++) {
+    const data = await getBookById(parsedDataEl[i]);
+    console.log(data);
+    bookDataArray.push(data);
+  }
+  markupBooks(bookDataArray);
 };
 
 getBookInfo();
+
+displayMarkupBasedOnLocalStorage();
