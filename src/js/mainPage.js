fe0f3b0c00d812a41c06ceefe3d8d89d4028c587
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2023-07-11 12:54:59 +0300

    fix: loader

--- src/js/mainPage.js
+++ src/js/mainPage.js
@@ -1,11 +1,9 @@
 import fillBestSellers from './fillBestSellers';
 import executeWithLoader from './service/executeWithLoader';
-import selectedCategoryMarkup from './categories/selectedcategorymarkup';
 import fillCategories from './categories_list';
-
-executeWithLoader(() => {
-  fillBestSellers();
-  fillCategories();
+executeWithLoader(async () => {
+  await fillBestSellers();
+  await fillCategories();
 });
 
-selectedCategoryMarkup();
+// selectedCategoryMarkup();
