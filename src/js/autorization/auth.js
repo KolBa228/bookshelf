bc0605548784f671dbc8724393fc5db58a25c236
Kilativ54 <vitalikechkalov@gmail.com>
2023-07-12 17:14:46 +0200

    try to add is-hidden header

--- src/js/autorization/auth.js
+++ src/js/autorization/auth.js
@@ -1,4 +1,3 @@
-
 // import {showCommentsSection, hideCommentsSection} from './app'
 // import { loadComments } from "./comments";
 
@@ -6,12 +5,12 @@
 //     const email = document.getElementById("email").value;
 //     const password = document.getElementById("password").value;
 //     const name = document.getElementById("name").value;
-  
+
 //     firebase.auth().createUserWithEmailAndPassword(email, password)
 //       .then(function(userCredential) {
 //         // Отримання зареєстрованого користувача
 //         const user = userCredential.user;
-  
+
 //         // Оновлення профілю користувача з ім'ям
 //         user.updateProfile({
 //           displayName: name
@@ -27,8 +26,6 @@
 //         console.log(error);
 //       });
 //   }
-  
-
 
 // export function signIn() {
 //   const email = document.getElementById("email").value;
@@ -54,67 +51,72 @@
 //     });
 // }
 
-
 // new Change
 
-
-import {showCommentsSection, hideCommentsSection} from './app'
-import { loadComments } from "./comments";
+let opnBtnModel = document.getElementById('opnBtnMod');
+import { showCommentsSection, hideCommentsSection } from './app';
+import { loadComments } from './comments';
 import { sendBtnOut } from './app';
 export function signUp() {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const name = document.getElementById("name").value;
-   
-      firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(function(userCredential) {
-        // Отримання зареєстрованого користувача
-        const user = userCredential.user;
-        sendBtnOut.classList.remove('is-display')
-        // Оновлення профілю користувача з ім'ям
-        user.updateProfile({
-          displayName: name
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  let name = document.getElementById('name').value;
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(function (userCredential) {
+      // Отримання зареєстрованого користувача
+      // console.log(opnBtnModel.textContent);
+      const user = userCredential.user;
+      sendBtnOut.classList.remove('is-display');
+      document.getElementById('header-auth').classList.remove('is-hidden');
+      // Оновлення профілю користувача з ім'ям
+      user
+        .updateProfile({
+          displayName: name,
         })
-        .then(function() {
+        .then(function () {
           showCommentsSection();
-          
         })
-        .catch(function(error) {
+        .catch(function (error) {
           console.log(error);
-          sendBtnOut.classList.add('is-display')
+          sendBtnOut.classList.add('is-display');
         });
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  }
-  
-
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
 
 export function signIn() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
   // sendBtnOut.classList.remove('is-display')
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(function() {
-       showCommentsSection();
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(function () {
+      showCommentsSection();
       loadComments();
-      sendBtnOut.classList.remove('is-display')
+      sendBtnOut.classList.remove('is-display');
+      document.getElementById('header-auth').classList.remove('is-hidden');
     })
-    .catch(function(error) {
+    .catch(function (error) {
       console.log(error);
-      sendBtnOut.classList.add('is-display')
+      sendBtnOut.classList.add('is-display');
     });
-    
 }
 
 export function signOut() {
-  firebase.auth().signOut()
-    .then(function() {
+  firebase
+    .auth()
+    .signOut()
+    .then(function () {
       hideCommentsSection();
-      sendBtnOut.classList.add('is-display')
-          })
-    .catch(function(error) {
+      sendBtnOut.classList.add('is-display');
+      document.getElementById('header-auth').classList.add('is-hidden');
+    })
+    .catch(function (error) {
       console.log(error);
     });
 }
