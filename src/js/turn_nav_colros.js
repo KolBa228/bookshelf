f3415178bdf939b1a50e0fcc80edc8d586da28eb
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-07-13 18:30:44 +0300

    chek js file

--- src/js/turn_nav_colros.js
+++ src/js/turn_nav_colros.js
@@ -4,15 +4,19 @@ const shopingModalIcon = document.querySelectorAll('.header-uli-svg')
 
 const pageTitle = document.title;
 function tooglePageTitle() {
+    console.log(window.location.pathname);
     if (window.location.pathname === '/shoping_list.html') {
         homeNavBtn.forEach(el => {
             el.classList.remove('active');
+            console.log('rwerwerwerwerwerwerweerwqrewrew');
         });
         shopingNavBtn.forEach(el => {
             el.classList.add('active');
+            console.log('rwerwerwerwerwerwerweerwqrewrew');
         });
         shopingModalIcon.forEach((el) => {
             el.classList.add('active-icon')
+            console.log('rwerwerwerwerwerwerweerwqrewrew');
         })
     }
     return;
