7e37093b7d9df66d3c16f86a02a59a3e3053d4d6
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-07-13 12:24:22 +0300

    fix modal svg

--- src/js/turn_nav_colros.js
+++ src/js/turn_nav_colros.js
@@ -1,17 +1,21 @@
 const homeNavBtn = document.querySelectorAll('.active-home');
 const shopingNavBtn = document.querySelectorAll('.active-shoping');
+const shopingModalIcon = document.querySelectorAll('.header-uli-svg')
 
 const pageTitle = document.title;
 function tooglePageTitle() {
-  if (window.location.pathname === '/shoping_list.html') {
-    homeNavBtn.forEach(el => {
-      el.classList.remove('active');
-    });
-    shopingNavBtn.forEach(el => {
-      el.classList.add('active');
-    });
-  }
-  return;
+    if (window.location.pathname === '/shoping_list.html') {
+        homeNavBtn.forEach(el => {
+            el.classList.remove('active');
+        });
+        shopingNavBtn.forEach(el => {
+            el.classList.add('active');
+        });
+        shopingModalIcon.forEach((el) => {
+            el.classList.add('active-icon')
+        })
+    }
+    return;
 }
 
 tooglePageTitle();
